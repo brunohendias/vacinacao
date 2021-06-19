d8521fcc4f490804389a3032ab94a05069e1a324
brunohendias <brunohendias@gmail.com>
2021-06-19 10:45:19 -0300

    Logging messages

---
+++ resources/lang/en/logging.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Message Logging
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during logging. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'emergency' => 'A service is no longer responding.',
+    'alert' => 'When performing an operation did not occur as expected.',
+    'critical' => 'The Application is in critical condition.',
+    'error' => 'An internal error occurred during processing.',
+    'warning' => 'We are close to making an error.',
+    'notice' => 'This operation may become an error.',
+    'info' => 'Success in performing an operation.',
+    'debug' => 'Information from an operation: ',
+];
---
+++ resources/lang/ptbr/logging.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Message Logging
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during logging. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'emergency' => 'Um serviço não esta mais respondendo.',
+    'alert' => 'Ao realizar uma operação não ocorreu o esperado.',
+    'critical' => 'A Aplicação esta em estado critico.',
+    'error' => 'Ocorreu um erro interno durante o processamento.',
+    'warning' => 'Estamos perto de acontecer um erro.',
+    'notice' => 'Essa operação pode se tornar um erro.',
+    'info' => 'Sucesso ao realizar uma operação.',
+    'debug' => 'Informações de uma operação: ',
+];
