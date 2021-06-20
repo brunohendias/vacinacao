85bdff9ad420c8e9bc8f22055dae4d185003a842
brunohendias <brunohendias@gmail.com>
2021-06-20 00:05:42 -0300

    errors, successo response

---
+++ resources/js/components/shared/panel/success.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-if="success" class="alert alert-success mt-2 col-sm-5">{{ success }}</div>
+</template>
+
+<script>
+export default {
+    name: 'alertSuccess',
+    props: {
+        success: String
+    }
+}
+</script>
