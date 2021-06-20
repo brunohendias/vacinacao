7afb8983a5e2f016cdd1f1c4940a142b6ef34096
brunohendias <brunohendias@gmail.com>
2021-06-20 00:07:44 -0300

    rotas update

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,17 @@ Route::get('/', function () {
 Route::prefix('fabricante')->group(function () {
     Route::get('/', 'FabricanteController@index')->name('fabricante.index');
     Route::post('/', 'FabricanteController@store');
+    Route::put('/{fabricante}', 'HistoricoController@update');
 });
 Route::prefix('vacina')->group(function () {
     Route::get('/', 'VacinaController@index')->name('vacina.index');
     Route::post('/', 'VacinaController@store');
+    Route::put('/{vacina}', 'HistoricoController@update');
 });
 Route::prefix('paciente')->group(function () {
     Route::get('/', 'PacienteController@index')->name('paciente.index');
     Route::post('/', 'PacienteController@store');
+    Route::put('/{paciente}', 'HistoricoController@update');
 });
 Route::prefix('historico')->group(function () {
     Route::get('/', 'HistoricoController@index')->name('historico.index');
